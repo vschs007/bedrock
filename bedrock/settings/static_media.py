94f7a4a859f07f47e2a1615034cac8eccecab1e5
Jon Petto <jonpetto@gmail.com>
2017-04-12 12:29:47 -0500

    Add sticky sub-nav.

--- bedrock/settings/static_media.py
+++ bedrock/settings/static_media.py
@@ -1460,6 +1460,14 @@ PIPELINE_JS = {
         ),
         'output_filename': 'js/geolocation-bundle.js',
     },
+    'firefox-hub-home': {
+        'source_filenames': (
+            'js/libs/jquery.waypoints.min.js',
+            'js/libs/jquery.waypoints-sticky.min.js',
+            'js/firefox/hub/home.js',
+        ),
+        'output_filename': 'js/firefox-hub-home-bundle.js',
+    },
     'home': {
         'source_filenames': (
             'js/libs/jquery.cycle2.min.js',
